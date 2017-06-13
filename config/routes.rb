ea5edc976db51698262b5a546b8422634f75d1d2
Alberto Aldana <albertoaldanar@hotmail.com>
2017-06-13 12:32:01 +0200

    seeds-request

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'reviews/new'
-
-  get 'reviews/create'
-
-=======
 
   resources :books
->>>>>>> b8c717f004b2454d69a25ff168cfba055f34e187
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
