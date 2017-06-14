8fc8a82946022ffcf5a40a4c7b91cee8576c891b
luisrog <luisrdzovejerogomez@gmail.com>
2017-06-14 15:29:35 +0200

    Fixing problem

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :book
   belongs_to :user
-  belongs_to :buyer, class: User, foreign_key: "buyer_user_id"
+  belongs_to :buyer, class_name: "User", foreign_key: "buyer_user_id"
 end
 
 
